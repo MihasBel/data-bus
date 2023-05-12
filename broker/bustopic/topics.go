d8394cdc70a8efb377d2aa01ab65a5ddbf98ce17
Mihas Generalis <kasinaki97@gmail.com>
2023-05-12 23:14:41 +0300

    Topics upd

--- broker/bustopic/topics.go
+++ broker/bustopic/topics.go
@@ -1,12 +1,23 @@
 package bustopic
 
 var (
-	Info   Topic = newTopic("info")
-	Errors Topic = newTopic("errors")
+	Info      Topic = newTopic("info")
+	Errors    Topic = newTopic("errors")
+	MsgTopics       = Topics{Info, Errors}
 )
 
 type (
-	Topic *string
+	Topic  *string
+	Topics []Topic
 )
 
 func newTopic(t string) *string { return &t }
+
+func IsInTopics(topicStr string) bool {
+	for _, t := range MsgTopics {
+		if *t == topicStr {
+			return true
+		}
+	}
+	return false
+}
